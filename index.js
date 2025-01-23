84416eaea6c173a7bf330bb98402e4c7d09f3c3d
Edith Gatwiri <edithgatwiri70@gmail.com>
2025-01-23 13:31:10 +0300

    tabnine

---
+++ index.js
@@ -0,0 +1,9 @@
+console.log("The script is running...");
+
+function calculateRectangleArea(length, width) {
+    return length * width;
+}
+const length = 5;
+const width = 10;
+const area = calculateRectangleArea(length, width);
+console.log(`The area of a rectangle with length ${length} and width ${width} is ${area} square units.`);
